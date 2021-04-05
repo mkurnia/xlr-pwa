6d09e09e2b0af54b9c387e36fa370d0498aa59b9
Muhammad Kurnia <muhammad.kurnia@larisin.id>
2021-04-05 13:58:54 +0700

    init

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  publicPath: "./",
+
+  pwa: {
+    name: "XLR PWA",
+    themeColor: "#42b983",
+    msTileColor: "#42b983",
+    appleMobileWebAppCache: "yes",
+    manifestOptions: {
+      background_color: "#42b983"
+    }
+  }
+};
